281ccef7375dce8f20ce3855fb53deeea2a83966
Anna Kate Zbitkovska <124578578+AnnaKate23@users.noreply.github.com>
2023-02-14 21:33:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def find_mismatch(text):
             # Process closing bracket, write your code here
             
         if opening_brackets_stack:
-           return opening_brackets_stack[-1].position
+            return opening_brackets_stack[-1].position
         return "Success"
 
 
